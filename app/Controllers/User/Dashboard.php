ef775d1c034e9466a70a90d13e47ff9eae5fb055
alifmusthofa <muhammad.alif.musthofa@students.amikom.ac.id>
2023-06-14 22:32:48 +0700

    Commit selanjutnua

--- app/Controllers/User/Dashboard.php
+++ app/Controllers/User/Dashboard.php
@@ -69,6 +69,7 @@ class Dashboard extends BaseController
     {
         $produk = new ProdukModel();
         $ukuran = new CategoryModel();
+        $ukuran = new CategoryModel();
         echo view('user/listBarang');
         echo view('user/listBarang');
     }
